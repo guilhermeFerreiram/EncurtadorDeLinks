3a182d76f7650326f1a3ee929fd226c52130cf4a
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-07-23 20:35:11 -0300

    Criar views e controller para Link

---
+++ EncurtadorDeLinks/Controllers/LinkController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EncurtadorDeLinks.Controllers
+{
+    public class LinkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Encurtar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
